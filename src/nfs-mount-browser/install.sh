357f842d41d712ea612adcd2f6b4c4e4bb26df75
Mario Bielert <mario@moonlake.de>
2009-11-06 21:05:28 +0100

    Added chmod +x to the install scripts.

--- src/nfs-mount-browser/install.sh
+++ src/nfs-mount-browser/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cp nfsmount.conf /etc/dbus-1/system.d/nfsmount.conf
 cp nfs-mount-browser /etc/init.d/nfs-mount-browser
+chmod +x /etc/init.d/nfs-mount-browser
 cp nfs-mount-browser.py /usr/local/sbin/nfs-mount-browser.py
+chmod +x /usr/local/sbin/nfs-mount-browser.py
 update-rc.d nfs-mount-browser defaults 90
 
--- src/nfs-mount-export/install.sh
+++ src/nfs-mount-export/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash	
 
 cp nfs-mount-export /etc/init.d/nfs-mount-export
+chmod +x /etc/init.d/nfs-mount-export
 cp nfs-mount-exporter.py /usr/local/sbin/nfs-mount-exporter.py
-
+chmod +x /usr/local/sbin/nfs-mount-exporter.py
 update-rc.d nfs-mount-export defaults 90
