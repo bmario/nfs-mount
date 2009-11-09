756300208dcbbe5ab47d12b5e33e6539f23b3bc9
Mario Bielert <mario@moonlake.de>
2009-11-09 01:35:29 +0100

    Recode nfs-mount-browser und systray to use the NfsMountShare class.

--- src/nfs-mount-browser/nfs-mount-browser.py
+++ src/nfs-mount-browser/nfs-mount-browser.py
@@ -25,161 +25,214 @@ import dbus, gobject, avahi, os, subprocess
 from dbus import DBusException
 import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
+DBusGMainLoop( set_as_default = True )
 
-class nfs_browser(dbus.service.Object):
-    def __init__(self):
-        DBusGMainLoop(set_as_default=True)
+
+class NfsMountShare:
+    def __init__( self, name, host, address, port, path, version ):
+        self.name = name
+        self.host = host
+        self.address = address
+        self.port = port
+        self.path = path
+        self.version = version
+
+    def getName( self ):
+        return self.name
+
+    def getHost( self ):
+        return self.host
+
+    def getAddress( self ):
+        return self.address
+
+    def getPort( self ):
+        return self.port
+
+    def getPath( self ):
+        return self.path
+
+    def getVersion( self ):
+        return self.version
+
+    def __str__( self ):
+        return self.getName()
+
+    def getMountCommand( self ):
+        if( self.version == "4" ):
+            return r"mount -t nfs4 %s:%s /media/nfs/%s%s" % ( self.address, self.path, self.host, self.path.replace( "/", "_" ) )
+        else:
+            return r"mount -t nfs %s:%s /media/nfs/%s%s" % ( self.address, self.path, self.host, self.path.replace( "/", "_" ) )
+
+    def getUnmountCommand( self ):
+        return r"umount -l -f /media/nfs/%s%s" % ( self.host, self.path.replace( "/", "_" ) )
+
+    def getMountDir( self ):
+        return r"/media/nfs/%s%s" % ( self.host, self.path.replace( "/", "_" ) )
+
+class nfs_browser( dbus.service.Object ):
+    def __init__( self ):
 
         # prepare own dbus interface
         self.session_bus = dbus.SystemBus()
-        name = dbus.service.BusName("de.moonlake.nfsmount", self.session_bus)
-        dbus.service.Object.__init__(self, dbus.SystemBus(), "/de/moonlake/nfsmount")
+        name = dbus.service.BusName( "de.moonlake.nfsmount", self.session_bus )
+        dbus.service.Object.__init__( self, dbus.SystemBus(), "/de/moonlake/nfsmount" )
 
         # prepare avahi
         self.bus = dbus.SystemBus()
-        self.server = dbus.Interface( self.bus.get_object(avahi.DBUS_NAME, '/'), 'org.freedesktop.Avahi.Server')
-        self.sbrowser = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME, self.server.ServiceBrowserNew(avahi.IF_UNSPEC, avahi.PROTO_INET, "_nfs._tcp", 'local', dbus.UInt32(0))), avahi.DBUS_INTERFACE_SERVICE_BROWSER)
-        self.sbrowser.connect_to_signal("ItemNew", self.newItem)
-        self.sbrowser.connect_to_signal("ItemRemove", self.removeItem)
+        self.server = dbus.Interface( self.bus.get_object( avahi.DBUS_NAME, '/' ), 'org.freedesktop.Avahi.Server' )
 
-        self.s4browser = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME, self.server.ServiceBrowserNew(avahi.IF_UNSPEC, avahi.PROTO_INET, "_nfs4._tcp", 'local', dbus.UInt32(0))), avahi.DBUS_INTERFACE_SERVICE_BROWSER)
-        self.s4browser.connect_to_signal("ItemNew", self.newItem)
-        self.s4browser.connect_to_signal("ItemRemove", self.removeItem)
+        # nfs3 scanning
+        self.sbrowser = dbus.Interface( self.bus.get_object( avahi.DBUS_NAME, self.server.ServiceBrowserNew( avahi.IF_UNSPEC, avahi.PROTO_INET, "_nfs._tcp", 'local', dbus.UInt32( 0 ) ) ), avahi.DBUS_INTERFACE_SERVICE_BROWSER )
+        self.sbrowser.connect_to_signal( "ItemNew", self.newItem )
+        self.sbrowser.connect_to_signal( "ItemRemove", self.removeItem )
 
-        
+        # nfs4 scanning
+        self.s4browser = dbus.Interface( self.bus.get_object( avahi.DBUS_NAME, self.server.ServiceBrowserNew( avahi.IF_UNSPEC, avahi.PROTO_INET, "_nfs4._tcp", 'local', dbus.UInt32( 0 ) ) ), avahi.DBUS_INTERFACE_SERVICE_BROWSER )
+        self.s4browser.connect_to_signal( "ItemNew", self.newItem )
+        self.s4browser.connect_to_signal( "ItemRemove", self.removeItem )
+
+
         self.shares = {}
         self.mounts = []
-        
+
         self.updateMountList()
-        
+
         # prepare mounting
         try:
-            os.makedirs(r"/media/nfs")
+            os.makedirs( r"/media/nfs" )
         except:
             pass
 
         gobject.MainLoop().run()
-        
-    @dbus.service.signal(dbus_interface='de.moonlake.nfsmount', signature='s')
-    def newShare(self, share):
-        pass
-        
-    @dbus.service.signal(dbus_interface='de.moonlake.nfsmount', signature='s')        
-    def removeShare(self, share):
-        pass
 
-    def newItem(self, interface, protocol, name, stype, domain, flags):
-        args = self.server.ResolveService(interface, protocol, name, stype, domain, avahi.PROTO_INET, dbus.UInt32(0))
+    def newItem( self, interface, protocol, name, stype, domain, flags ):
+        args = self.server.ResolveService( interface, protocol, name, stype, domain, avahi.PROTO_INET, dbus.UInt32( 0 ) )
 
         txt = ''
         for i in args[9][0]:
-            txt = "%s%s" % (txt, i)
-        
-        if(stype=="_nfs4._tcp"):
+            txt = "%s%s" % ( txt, i )
+
+        if( stype == "_nfs4._tcp" ):
             version = "4"
         else:
             version = "3"
-                
-        name = str(name)
-        host = str(args[5])
-        address = str(args[7])
-        port = str(args[8])
+
+        name = str( name )
+        host = str( args[5] )
+        address = str( args[7] )
+        port = str( args[8] )
         path = txt[5:]
-        self.shares[name] = [name, host, address, port, path, version]
-        
+
+        # create new share class and store it
+        self.shares[name] = NfsMountShare( name, host, address, port, path, version )
+
+        # emit signal "newShare"
+        self.newShare( name )
+
+        # looking for mounts allready done, outside from me
         self.updateMountList()
-        
-        self.newShare(name)
-        
-    def removeItem(self, interface, protocol, name, stype, domain, flags):
-        if self.isShareMounted(name):
-            self.unmountShare(name)
+
+    def removeItem( self, interface, protocol, name, stype, domain, flags ):
+        if self.isShareMounted( name ):
+            self.unmountShare( name )
         if name in self.shares:
             del self.shares[name]
-        self.removeShare(name)
+        self.removeShare( name )
 
-    @dbus.service.method(dbus_interface='de.moonlake.nfsmount', in_signature='s', out_signature='as')
-    def getShareInfo(self, share):
-        return self.shares[share]
+    @dbus.service.signal( dbus_interface = 'de.moonlake.nfsmount', signature = 's' )
+    def newShare( self, share ):
+        pass
 
-    @dbus.service.method(dbus_interface='de.moonlake.nfsmount', in_signature='s', out_signature='b')
-    def isShareMounted(self, share):
+    @dbus.service.signal( dbus_interface = 'de.moonlake.nfsmount', signature = 's' )
+    def removeShare( self, share ):
+        pass
+
+    @dbus.service.method( dbus_interface = 'de.moonlake.nfsmount', in_signature = 's', out_signature = 'as' )
+    def getShareInfo( self, share ):
+        share = self.shares[share]
+        return [share.getName(), share.getHost(), share.getAddress(), share.getPort(), share.getPath(), share.getVersion()]
+
+    @dbus.service.method( dbus_interface = 'de.moonlake.nfsmount', in_signature = 's', out_signature = 'b' )
+    def isShareMounted( self, share ):
         if share in self.mounts:
             return True
         else:
             return False
 
-    @dbus.service.method(dbus_interface='de.moonlake.nfsmount', in_signature='s', out_signature='')
-    def mountShare(self, share):
-        share = self.getShareInfo(share)
+    @dbus.service.method( dbus_interface = 'de.moonlake.nfsmount', in_signature = 's', out_signature = '' )
+    def mountShare( self, share ):
         try:
-            os.makedirs(r"/media/nfs/%s%s" % (share[1], share[4].replace("/", "_")))
+            share = self.shares[share]
         except:
             pass
-        if(share[5] == "4"):
-            os.system(r"mount -t nfs4 %s:%s /media/nfs/%s%s" % (share[2], share[4], share[1], share[4].replace("/", "_")))
-        else:
-            os.system(r"mount -t nfs %s:%s /media/nfs/%s%s" % (share[2], share[4], share[1], share[4].replace("/", "_")))
-        
-        self.updateMountList()
-        
-    @dbus.service.method(dbus_interface='de.moonlake.nfsmount', in_signature='s', out_signature='')
-    def unmountShare(self, share):
-        share = self.getShareInfo(share)
-        os.system(r"umount -l -f /media/nfs/%s%s" % (share[1], share[4].replace("/", "_")))
-        os.rmdir(r"/media/nfs/%s%s" % (share[1], share[4].replace("/", "_")))
+        try:
+            os.makedirs( share.getMountDir() )
+            os.system( share.getMountCommand() )
+        except:
+            pass
+
         self.updateMountList()
 
-    @dbus.service.signal(dbus_interface='de.moonlake.nfsmount', signature='s')        
-    def mountedShare(self, share):
+    @dbus.service.method( dbus_interface = 'de.moonlake.nfsmount', in_signature = 's', out_signature = '' )
+    def unmountShare( self, share ):
+        try:
+            share = self.shares[share]
+        except:
+            pass
+        try:
+            os.system( share.getUnmountCommand() )
+            os.rmdir( share.getMountDir() )
+        except:
+            pass
+        self.updateMountList()
+
+    @dbus.service.signal( dbus_interface = 'de.moonlake.nfsmount', signature = 's' )
+    def mountedShare( self, share ):
         pass
 
-    @dbus.service.signal(dbus_interface='de.moonlake.nfsmount', signature='s')        
-    def unmountedShare(self, share):
+    @dbus.service.signal( dbus_interface = 'de.moonlake.nfsmount', signature = 's' )
+    def unmountedShare( self, share ):
         pass
-        
-    @dbus.service.method(dbus_interface='de.moonlake.nfsmount', in_signature='', out_signature='as')
-    def getShareList(self):
+
+    @dbus.service.method( dbus_interface = 'de.moonlake.nfsmount', in_signature = '', out_signature = 'as' )
+    def getShareList( self ):
         return self.shares.keys()
 
-    @dbus.service.method(dbus_interface='de.moonlake.nfsmount', in_signature='', out_signature='as')
-    def getMountList(self):
+    @dbus.service.method( dbus_interface = 'de.moonlake.nfsmount', in_signature = '', out_signature = 'as' )
+    def getMountList( self ):
         return self.mounts
 
-    @dbus.service.method(dbus_interface='de.moonlake.nfsmount', in_signature='', out_signature='')
-    def updateMounts(self):
+    @dbus.service.method( dbus_interface = 'de.moonlake.nfsmount', in_signature = '', out_signature = '' )
+    def updateMounts( self ):
         self.updateMountList()
-    
-    def print_error(*args):
-        print 'error_handler'
-        print args[0]
-        
-    def updateMountList(self, filename='/etc/mtab'):
+
+    def updateMountList( self, filename = '/etc/mtab' ):
         oldmounts = self.mounts
         self.mounts = []
-        f = open(filename, 'r')
+        f = open( filename, 'r' )
         for line in f.readlines():
-            if(line.find('nfsd') >= 0): continue # don't care about nfsd pseudo file system
-            if(line.find(' nfs') == -1): continue # we just need nfs or nfs4 file systems
-            address = line[:line.find(":")]
-            path = line[line.find(":") + 1:line.find(" ")]
-            
+            if( line.find( 'nfsd' ) >= 0 ): continue # don't care about nfsd pseudo file system
+            if( line.find( ' nfs' ) == -1 ): continue # we just need nfs or nfs4 file systems
+            address = line[:line.find( ":" )]
+            path = line[line.find( ":" ) + 1:line.find( " " )]
+
             # iter all shares
             for share in self.shares.itervalues():
-                if(share[2] == address and share[4] == path):
+                if( share.getAddress() == address and share.getPath() == path ):
                     # mount matches known share
-                    self.mounts.append(share[0])
-                    
+                    self.mounts.append( share.getName() )
+
                     # if new trigger signal
-                    if(share[0] not in oldmounts):
-                        self.mountedShare(share[0])
+                    if( share.getName() not in oldmounts ):
+                        self.mountedShare( share.getName() )
                     # if share not new, remove it from oldmountlist, so we know, which mounts are unmounted
-                    if(share[0] in oldmounts):
-                        oldmounts.remove(share[0])
+                    if( share.getName() in oldmounts ):
+                        oldmounts.remove( share.getName() )
         f.close()
 
         for share in oldmounts:
-            self.unmountedShare(share)
+            self.unmountedShare( share )
 
 if __name__ == "__main__":
     nfs = nfs_browser()
--- src/nfs-mount-systray/nfs-mount-systray.py
+++ src/nfs-mount-systray/nfs-mount-systray.py
@@ -25,7 +25,7 @@ import dbus
 import avahi
 from sys import exit
 from dbus.mainloop.glib import DBusGMainLoop
-DBusGMainLoop(set_as_default=True)
+DBusGMainLoop( set_as_default = True )
 try:
     import pynotify
 except:
@@ -33,178 +33,218 @@ except:
     print "So there are no notifications."
     print "To change this, just install python-notify."
 
+class NfsMountShare:
+    def __init__( self, name, host, address, port, path, version ):
+        self.name = name
+        self.host = host
+        self.address = address
+        self.port = port
+        self.path = path
+        self.version = version
+
+    def getName( self ):
+        return self.name
+
+    def getHost( self ):
+        return self.host
+
+    def getAddress( self ):
+        return self.address
+
+    def getPort( self ):
+        return self.port
+
+    def getPath( self ):
+        return self.path
+
+    def getVersion( self ):
+        return self.version
+
+    def __str__( self ):
+        return self.getName()
+
 class NfsMountSystrayApplet:
-    def __init__(self):
+    def __init__( self ):
         # set notification
-        client = gconf.client_get_default()
-        client.add_dir('/apps/nfs-mount-systray/notifications', gconf.CLIENT_PRELOAD_NONE)
-        client.add_dir('/apps/nfs-mount-systray/shares', gconf.CLIENT_PRELOAD_NONE)
-        if(client.get_bool('/apps/nfs-mount-systray/init') == 0):
-        	client.set_bool('/apps/nfs-mount-systray/notifications/all', 1)
-        	client.set_bool('/apps/nfs-mount-systray/init', 1)
-        	self.notify = True
-        else:
-        	self.notify = client.get_bool('/apps/nfs-mount-systray/notifications/all')
+        self.load_notify_conf()
 
         # set icon
-        self.icon = gtk.status_icon_new_from_icon_name("server")
+        self.icon = gtk.status_icon_new_from_icon_name( "server" )
 
         # connect to signals        
-        self.icon.connect("popup-menu", self.show_menu)
-        self.icon.connect("activate", self.show_shares)
+        self.icon.connect( "popup-menu", self.show_menu )
+        self.icon.connect( "activate", self.show_shares )
 
         # dbus stuff
         try:
             bus = dbus.SystemBus()
-            self.nfsmount = dbus.Interface(bus.get_object('de.moonlake.nfsmount', '/de/moonlake/nfsmount'), 'de.moonlake.nfsmount')
+            self.nfsmount = dbus.Interface( bus.get_object( 'de.moonlake.nfsmount', '/de/moonlake/nfsmount' ), 'de.moonlake.nfsmount' )
         except:
             print "Could not connect to the nfs-mount-browser daemon over DBus. Please make sure, that the daemon is running."
-            exit(0)
+            exit( 0 )
 
         # set tooltip
         self.update_tooltip()
-            
+
         # connect to dbus signals
-        self.nfsmount.connect_to_signal("newShare", self.new_share)
-        self.nfsmount.connect_to_signal("removeShare", self.remove_share)
-        self.nfsmount.connect_to_signal("mountedShare", self.mounted_share)
-        self.nfsmount.connect_to_signal("unmountedShare", self.unmounted_share)
+        self.nfsmount.connect_to_signal( "newShare", self.new_share )
+        self.nfsmount.connect_to_signal( "removeShare", self.remove_share )
+        self.nfsmount.connect_to_signal( "mountedShare", self.mounted_share )
+        self.nfsmount.connect_to_signal( "unmountedShare", self.unmounted_share )
 
         # initial scan for shares
         for share in self.nfsmount.getShareList():
-            self.new_share(share)
+            self.new_share( share )
         for share in self.nfsmount.getMountList():
-            self.mounted_share(share)
+            self.mounted_share( share )
 
         gtk.main()
 
-    # quit methode
-    def quit(self,widget,event=None):
-        gtk.main_quit()
-
-    # toggle notifications
-    def toggle_notify(self, widget, event=None):
-    	client = gconf.client_get_default()
-        self.notify = not self.notify
-    	if(self.notify):
-    		client.set_bool('/apps/nfs-mount-systray/notifications/all', 1)
-    	else:
-    		client.set_bool('/apps/nfs-mount-systray/notifications/all', 0)
+    def load_notify_conf( self ):
+        client = gconf.client_get_default()
+        client.add_dir( '/apps/nfs-mount-systray/notifications', gconf.CLIENT_PRELOAD_NONE )
+        client.add_dir( '/apps/nfs-mount-systray/shares', gconf.CLIENT_PRELOAD_NONE )
+        # first run?
+        if( client.get_bool( '/apps/nfs-mount-systray/init' ) == 0 ):
+            # set standard values
+            client.set_bool( '/apps/nfs-mount-systray/notifications/all', 1 )
+            client.set_bool( '/apps/nfs-mount-systray/init', 1 )
+            self.notify = True
+        else:
+            # load old value
+            self.notify = client.get_bool( '/apps/nfs-mount-systray/notifications/all' )
 
     # unmount handler
-    def unmount_share(self, widget, event=None):
+    def unmount_share( self, widget, event = None ):
         if event in self.nfsmount.getMountList():
             try:
-                self.nfsmount.unmountShare(event)
+                self.nfsmount.unmountShare( event )
 
             except:
                 print "Disconnected from the DBus interface of nfs-mount-browser. Make sure, that this daemon runs."
-                exit(0) 
+                exit( 0 )
     # mount handler    
-    def mount_share(self, widget, event=None):
+    def mount_share( self, widget, event = None ):
         if event not in self.nfsmount.getMountList():
             try:
-                self.nfsmount.mountShare(event)
+                self.nfsmount.mountShare( event )
 
             except:
                 print "Disconnected from the DBus interface of nfs-mount-browser. Make sure, that this daemon runs."
-                exit(0) 
+                exit( 0 )
 
 
-    def new_share(self, share):
-    	share = self.nfsmount.getShareInfo(share)
+    def new_share( self, share ):
+    	share = self.nfsmount.getShareInfo( share )
 
     	self.update_tooltip()
-    	self.notify_user("New Nfs Share Found", "share name: <b>%s</b>\nhost: <b>%s</b>\naddress: <b>%s</b>\npath: <b>%s</b>" % (share[0], share[1], share[2], share[4]))
+    	self.notify_user( "New Nfs Share Found", "share name: <b>%s</b>\nhost: <b>%s</b>\naddress: <b>%s</b>\npath: <b>%s</b>" % ( share[0], share[1], share[2], share[4] ) )
 
     	client = gconf.client_get_default()
-    	client.add_dir('/apps/nfs-mount-systray/shares/%s' % share[0].replace("/","_"), gconf.CLIENT_PRELOAD_NONE)
-    	if client.get_bool('/apps/nfs-mount-systray/shares/%s/automount' % share[0].replace("/","_")) == 1:
-    		self.nfsmount.mountShare(share[0])
+    	client.add_dir( '/apps/nfs-mount-systray/shares/%s' % share[0].replace( "/", "_" ), gconf.CLIENT_PRELOAD_NONE )
+    	if client.get_bool( '/apps/nfs-mount-systray/shares/%s/automount' % share[0].replace( "/", "_" ) ) == 1:
+    		self.nfsmount.mountShare( share[0] )
         else:
-        	client.set_bool('/apps/nfs-mount-systray/shares/%s/automount' % share[0].replace("/","_"), 0)
+        	client.set_bool( '/apps/nfs-mount-systray/shares/%s/automount' % share[0].replace( "/", "_" ), 0 )
 
-    def remove_share(self, share):
-        self.sharelist.remove(share)
+    def remove_share( self, share ):
+        self.sharelist.remove( share )
         self.update_tooltip()
-        self.notify_user("Nfs Share has been removed", "share name: <b>%s</b>" % share)
+        self.notify_user( "Nfs Share has been removed", "share name: <b>%s</b>" % share )
 
-    def mounted_share(self, share):
+    def mounted_share( self, share ):
         self.update_tooltip()
-        self.notify_user("Nfs Share has been mounted", "share name: <b>%s</b>" % share)
+        self.notify_user( "Nfs Share has been mounted", "share name: <b>%s</b>" % share )
 
-    def unmounted_share(self, share):
+    def unmounted_share( self, share ):
         self.update_tooltip()
-        self.notify_user("Nfs Share has been unmounted", "share name: <b>%s</b>" % share)
+        self.notify_user( "Nfs Share has been unmounted", "share name: <b>%s</b>" % share )
 
 
-    def update_tooltip(self, message=None):
-        tooltip = "Nfs Browser\n%i shares found.\n%i shares mounted." % (len(self.nfsmount.getShareList()), len(self.nfsmount.getMountList()))
-        if (message != None):
-            tooltip = message + "\n" + tooltip
-        self.icon.set_tooltip(tooltip)
-        
-    def show_menu(self, icon, button, timer):
-        self.nfsmount.updateMounts()
+    def update_tooltip( self ):
+        tooltip = "Nfs Browser\n%i shares found.\n%i shares mounted." % ( len( self.nfsmount.getShareList() ), len( self.nfsmount.getMountList() ) )
+        self.icon.set_tooltip( tooltip )
+
+    def show_menu( self, icon, button, timer ):
+        try:
+            self.nfsmount.updateMounts()
+        except:
+            print "Disconnected from the DBus interface of nfs-mount-browser. Make sure, that this daemon runs."
+            exit( 0 )
+
         menu = gtk.Menu()
         i = 0
         for share in self.nfsmount.getShareList():
             item = gtk.MenuItem()
-            hbox = gtk.HBox(False,12)
-            item.add(hbox)
+            hbox = gtk.HBox( False, 12 )
+            item.add( hbox )
             try:
-                si = self.nfsmount.getShareInfo(share)
+                si = self.nfsmount.getShareInfo( share )
+                share = NfsMountShare( si[0], si[1], si[2], si[3], si[4], si[5] )
             except:
                 print "Disconnected from the DBus interface of nfs-mount-browser. Make sure, that this daemon runs."
-                exit(0) 
-                
-            if share in self.nfsmount.getMountList():
-                hbox.pack_start(gtk.image_new_from_stock(gtk.STOCK_CONNECT,gtk.ICON_SIZE_MENU),False,False)
-                item.connect("activate",self.unmount_share, share)
-                tooltip = "Hostname: %s\nAddress: %s\nPath: %s\nVersion: %s\nShare is mounted." % (si[1], si[2],si[4], si[5])
+                exit( 0 )
+
+            if share.getName() in self.nfsmount.getMountList():
+                hbox.pack_start( gtk.image_new_from_stock( gtk.STOCK_CONNECT, gtk.ICON_SIZE_MENU ), False, False )
+                item.connect( "activate", self.unmount_share, share.getName() )
+                tooltip = "Hostname: %s\nAddress: %s\nPath: %s\nVersion: %s\nShare is mounted." % ( share.getHost(), share.getAddress(), share.getPath(), share.getVersion() )
             else:
-                hbox.pack_start(gtk.image_new_from_stock(gtk.STOCK_DISCONNECT,gtk.ICON_SIZE_MENU),False,False)
-                item.connect("activate",self.mount_share, share)
-                tooltip = "Hostname: %s\nAddress: %s\nPath: %s\nVersion: %s\nShare is not mounted" % (si[1], si[2], si[4],si[5])
-            label = gtk.Label(share)
-            label.set_alignment(1,1)
-            hbox.pack_start(label,False,False)
-            info = gtk.image_new_from_stock(gtk.STOCK_INFO,gtk.ICON_SIZE_MENU)
-            info.set_tooltip_text(tooltip)
-            info.set_alignment(1,1)
-            hbox.pack_start(info,True,True)            
-            menu.attach(item,0,1,i,i+1)
+                hbox.pack_start( gtk.image_new_from_stock( gtk.STOCK_DISCONNECT, gtk.ICON_SIZE_MENU ), False, False )
+                item.connect( "activate", self.mount_share, share.getName() )
+                tooltip = "Hostname: %s\nAddress: %s\nPath: %s\nVersion: %s\nShare is not mounted" % ( share.getHost(), share.getAddress(), share.getPath(), share.getVersion() )
+            label = gtk.Label( share.getName() )
+            label.set_alignment( 1, 1 )
+            hbox.pack_start( label, False, False )
+            info = gtk.image_new_from_stock( gtk.STOCK_INFO, gtk.ICON_SIZE_MENU )
+            info.set_tooltip_text( tooltip )
+            info.set_alignment( 1, 1 )
+            hbox.pack_start( info, True, True )
+            menu.attach( item, 0, 1, i, i + 1 )
             i = i + 1
-        menu.attach(gtk.MenuItem(),0,1,i,i+1)            
+        menu.attach( gtk.MenuItem(), 0, 1, i, i + 1 )
 
         # notification switch
         i = i + 1
-        item = gtk.CheckMenuItem("Notifications")
-        item.set_active(self.notify)
-        item.connect("activate",self.toggle_notify)
-        menu.attach(item,0,1,i,i+1)
+        item = gtk.CheckMenuItem( "Notifications" )
+        item.set_active( self.notify )
+        item.connect( "activate", self.toggle_notify )
+        menu.attach( item, 0, 1, i, i + 1 )
 
         # quit button
         i = i + 1
-        item = gtk.ImageMenuItem(gtk.STOCK_QUIT)
-        item.connect("activate",self.quit)
-        menu.attach(item,0,1,i,i+1)
-        
-            
+        item = gtk.ImageMenuItem( gtk.STOCK_QUIT )
+        item.connect( "activate", self.quit )
+        menu.attach( item, 0, 1, i, i + 1 )
+
+
         menu.show_all()
-        menu.popup(None, None, gtk.status_icon_position_menu, button, timer, icon)
+        menu.popup( None, None, gtk.status_icon_position_menu, button, timer, icon )
         self.update_tooltip()
-        
-    def show_shares(self, icon):
-        self.update_tooltip()        
-        
-    def notify_user(self, title, message):
+
+    # quit methode
+    def quit( self, widget, event = None ):
+        gtk.main_quit()
+
+    # toggle notifications
+    def toggle_notify( self, widget, event = None ):
+        client = gconf.client_get_default()
+        self.notify = not self.notify
+        if( self.notify ):
+            client.set_bool( '/apps/nfs-mount-systray/notifications/all', 1 )
+        else:
+            client.set_bool( '/apps/nfs-mount-systray/notifications/all', 0 )
+
+    def show_shares( self, icon ):
+        self.update_tooltip()
+
+    def notify_user( self, title, message ):
         if self.notify:
             # notify me :)
             try:
                 # use pynotify or nothing!
-                pynotify.init("Nfs Browser")
-                notification = pynotify.Notification(title, message, "server")
+                pynotify.init( "Nfs Browser" )
+                notification = pynotify.Notification( title, message, "server" )
                 #notification.attach_to_status_icon(self.icon)
                 notification.show()
             except:
